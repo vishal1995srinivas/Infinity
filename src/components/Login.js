16fa5ce2e7ceaf976f6ff8f95031c5a08d653c5d
Vishal Srinivas <vishal1995srinivas@gmail.com>
2020-03-09 12:24:11 +0530

    Login Warnings cleared

--- src/components/Login.js
+++ src/components/Login.js
@@ -42,55 +42,44 @@ class Login extends Component {
 			});
 	};
 	render() {
-		// console.log(this.state);
 		return (
-			// <div>
-			// 	<form onSubmit={(e) => this.onSubmit(e)}>
-			// 		<label>email</label>
-			// 		<input type="text" name="email" onChange={this.onChange} value={this.state.email} />
-			// 		<label>Password</label>
-			// 		<input type="password" name="password" onChange={this.onChange} value={this.state.password} />
-			// 		<button type="submit">submit</button>
-			// 	</form>
-			// </div>
-
-			<div class="row mt-5">
-				<div class="col-md-6 m-auto">
-					<div class="card card-body">
-						<h1 class="text-center mb-3">
-							<i class="fas fa-sign-in-alt" /> Login
+			<div className="row mt-5">
+				<div className="col-md-6 m-auto">
+					<div className="card card-body">
+						<h1 className="text-center mb-3">
+							<i className="fas fa-sign-in-alt" /> Login
 						</h1>
 
 						<form onSubmit={(e) => this.onSubmit(e)}>
-							<div class="form-group">
-								<label for="email">Email</label>
+							<div className="form-group">
+								<label htmlFor="email">Email</label>
 								<input
 									type="email"
 									id="email"
 									name="email"
-									class="form-control"
+									className="form-control"
 									placeholder="Enter Email"
 									onChange={this.onChange}
 									value={this.state.email}
 								/>
 							</div>
-							<div class="form-group">
-								<label for="password">Password</label>
+							<div className="form-group">
+								<label htmlFor="password">Password</label>
 								<input
 									type="password"
 									id="password"
 									name="password"
-									class="form-control"
+									className="form-control"
 									placeholder="Enter Password"
 									onChange={this.onChange}
 									value={this.state.password}
 								/>
 							</div>
-							<button type="submit" class="btn btn-primary btn-block">
+							<button type="submit" className="btn btn-primary btn-block">
 								Login
 							</button>
 						</form>
-						<p class="lead mt-4">
+						<p className="lead mt-4">
 							No Account? <a href="/register">Register</a>
 						</p>
 					</div>
