fe3e30e0e78b13be6b645d6629942a89fe3015b0
Vishal Srinivas <vishal1995srinivas@gmail.com>
2020-03-25 17:54:35 +0530

    Removed all the files which has been  moved to client/

--- src/components/Home.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Home = () => {
-	return <div>Hello World from Home</div>;
-};
-
-export default Home;
--- src/components/postman/Components/display/main/response/ResponseComponent.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class ResponseComponent extends Component {
-  render() {
-    return <div className="response">response</div>;
-  }
-}
-
-export default ResponseComponent;
