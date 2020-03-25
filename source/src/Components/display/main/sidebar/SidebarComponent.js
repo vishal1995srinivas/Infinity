73b7b8e13c85d01cf4dc216cb3d4faae8d2f558e
Vishal Srinivas <vishal1995srinivas@gmail.com>
2020-03-25 17:13:35 +0530

    Deleted old source files

--- source/src/Components/display/main/sidebar/SidebarComponent.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-export class SidebarComponent extends Component {
-  render() {
-    return <div className="sidebar">sidebar</div>;
-  }
-}
-
-export default SidebarComponent;
--- source/src/Components/header/HeaderComponent.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-export class HeaderComponent extends Component {
-  render() {
-    return <div className="header">POSTMAN</div>;
-  }
-}
-
-export default HeaderComponent;
