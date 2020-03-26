18397d19d9eadb34f5663b1f7cbeb716f45088f0
Vishal Srinivas <vishal1995srinivas@gmail.com>
2020-03-26 13:31:30 +0530

    Final commit

---
+++ build/precache-manifest.d8d201a9766fb4d80e786de1301879eb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "510a0598fd2775453cc9f5c13e815a1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7813f29a6ae0127490a2",
+    "url": "/static/css/2.f5298653.chunk.css"
+  },
+  {
+    "revision": "94b892881c1369a28250",
+    "url": "/static/css/main.87a120e3.chunk.css"
+  },
+  {
+    "revision": "7813f29a6ae0127490a2",
+    "url": "/static/js/2.eae60bce.chunk.js"
+  },
+  {
+    "revision": "0e97e94c3d1f2f28fce17fe6cde092ec",
+    "url": "/static/js/2.eae60bce.chunk.js.LICENSE"
+  },
+  {
+    "revision": "94b892881c1369a28250",
+    "url": "/static/js/main.07096c61.chunk.js"
+  },
+  {
+    "revision": "bc1f5d2574a77633a159",
+    "url": "/static/js/runtime-main.10ce9c8c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
