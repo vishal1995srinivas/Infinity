d282b62fa42a0bd139db639963b165bfc54e6f5c
Vishal Srinivas <vishal1995srinivas@gmail.com>
2020-03-27 16:53:03 +0530

    Deleted unnecessary files

--- routes/users.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../app/api/controllers/users');
-
-router.post('/register', userController.validate('createUser'), userController.create);
-router.post('/authenticate', userController.validate('loginUser'), userController.authenticate);
-
-module.exports = router;
