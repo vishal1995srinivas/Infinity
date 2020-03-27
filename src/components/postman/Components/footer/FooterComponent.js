0befacbfcb156003e257c249339a3635feb01a67
Vishal Srinivas <vishal1995srinivas@gmail.com>
2020-03-27 17:09:24 +0530

    deleted  unnecessary files

--- src/components/postman/Components/footer/FooterComponent.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-export class FooterComponent extends Component {
-	render() {
-		return <div className="footer">Created with 💝 by Vishal Srinivas</div>;
-	}
-}
-
-export default FooterComponent;
