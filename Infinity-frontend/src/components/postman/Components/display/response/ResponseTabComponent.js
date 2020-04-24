79366f558e48a23be4b0420ca80b66d091ac4c73
Vishal Srinivas <vishal1995srinivas@gmail.com>
2020-04-24 12:50:25 +0530

    response refactored

--- Infinity-frontend/src/components/postman/Components/display/response/ResponseTabComponent.js
+++ Infinity-frontend/src/components/postman/Components/display/response/ResponseTabComponent.js
@@ -102,33 +102,7 @@ class ResponseTabComponent extends Component {
 				});
 		}
 	}
-	async fetchData(url = '', data = {}, method, myHeaders) {
-		// Default options are marked with *
-		const response = await fetch(url, {
-			method: method, // *GET, POST, PUT, DELETE, etc.
-			mode: 'cors', // no-cors, *cors, same-origin
-			cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-			credentials: 'same-origin', // include, *same-origin, omit
-			headers: myHeaders,
-			redirect: 'follow', // manual, *follow, error
-			referrerPolicy: 'no-referrer', // no-referrer, *client
-			body: JSON.stringify(data) // body data type must match "Content-Type" header
-		});
-		return await response.json(); // parses JSON response into native JavaScript objects
-	}
-	async GetData(url = '', method, myHeaders) {
-		// Default options are marked with *
-		const response = await fetch(url, {
-			method: method, // *GET, POST, PUT, DELETE, etc.
-			mode: 'cors', // no-cors, *cors, same-origin
-			cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-			credentials: 'same-origin', // include, *same-origin, omit
-			headers: myHeaders,
-			redirect: 'follow', // manual, *follow, error
-			referrerPolicy: 'no-referrer' // no-referrer, *client
-		});
-		return await response.json(); // parses JSON response into native JavaScript objects
-	}
+
 	render() {
 		const { url } = this.props;
 		const { isLoading, error, JsonData, urlString, method, headers, bodyFormOrUrlData } = this.state;
